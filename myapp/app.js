b2e30399a9f5ff4661b1e3a2c3bc440b08b25508
monat_son <sonynakayama@gmail.com>
2022-12-14 12:20:01 +0100

    added functionality to add data to mondodb database
    Signed-off-by: monat_son <sonynakayama@gmail.com>

--- myapp/app.js
+++ myapp/app.js
@@ -3,7 +3,14 @@ const app = express();
 const router = express.Router();
 const path = require('path');
 const mongodb = require("mongodb");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const assert = require('assert');
+const bodyParser = require('body-parser');
+const url = "mongodb+srv://sony-admin:admin1234@cluster0.hxwuk9f.mongodb.net/persons"
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true });
 
 // serving static js scripts
 app.use(express.static('./public'));
@@ -15,7 +22,8 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
     console.log("server running on 3000")
-})
+});
+
 
 // creating schema for mongo
 const personSchema = {
@@ -27,4 +35,13 @@ const personSchema = {
 
 const Person = mongoose.model("Person", personSchema);
 
-
+app.post("/", (req, res) => {
+    let newPerson = new Person({
+        name: req.body.name,
+        surname: req.body.surname,
+        age: req.body.age,
+        phone: req.body.phone
+    })
+    newPerson.save();
+    res.redirect("/")
+})
